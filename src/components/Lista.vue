a55e2ea3916c5346fc2e65c55f82047a8a119b85
rd7r <sinanipaxir@gmail.com>
2021-09-21 21:15:40 -0400

    manejo de store

--- src/components/Lista.vue
+++ src/components/Lista.vue
@@ -1,32 +1,33 @@
 <template>
-  <div>
-    <ol class="list-group list-group-numbered">
-      <li class="list-group-item d-flex justify-content-between align-items-start">
+  <div class="col-lg-6 offset-3">
+    <ul class="list-group list-group-numbered">
+      <li v-for="(item, index) in frutas" :key="item.index" @click="aumentar(index)" 
+      class="list-group-item d-flex justify-content-between align-items-start flecha">
         <div class="ms-2 me-auto">
-          <div class="fw-bold">Subheading</div>
-          Cras justo odio
+          <div class="fw-bold">{{item.name}}</div>
         </div>
-        <span class="badge bg-primary rounded-pill">14</span>
+        <span class="badge bg-primary rounded-pill">{{item.cantidad}}</span>
       </li>
-      <li class="list-group-item d-flex justify-content-between align-items-start">
-        <div class="ms-2 me-auto">
-          <div class="fw-bold">Subheading</div>
-          Cras justo odio
-        </div>
-        <span class="badge bg-primary rounded-pill">14</span>
-      </li>
-      <li class="list-group-item d-flex justify-content-between align-items-start">
-        <div class="ms-2 me-auto">
-          <div class="fw-bold">Subheading</div>
-          Cras justo odio
-        </div>
-        <span class="badge bg-primary rounded-pill">14</span>
-      </li>
-    </ol>
+    </ul>
+    <div class="d-grid gap-2">
+      <button class="btn btn-primary btn-block mt-5" @click="reiniciar()" type="submit">Reset</button>
+    </div>
   </div>
 </template>
 <script>
+import { mapState, mapMutations } from 'vuex';
 export default {
   name: "Lista",
+  computed: {
+    ...mapState(['frutas'])
+  },
+  methods: {
+    ...mapMutations(['aumentar','reiniciar'])
+  },
 };
 </script>
+<style scoped>
+  .flecha{
+    cursor: pointer;
+  }
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,19 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    frutas: [
+      {name: 'Piña', cantidad: 0},
+      {name: 'Uva', cantidad: 0},
+      {name: 'Tuna', cantidad: 0}
+    ]
   },
   mutations: {
+    aumentar(state, index){
+      state.frutas[index].cantidad ++;
+    },
+    reiniciar(state){
+      state.frutas.map((fru)=>{fru.cantidad=0});
+    }
   },
   actions: {
   },
